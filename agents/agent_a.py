5fd654ab922df3c690fea51a2f08ae55f494f2e2
pranavpande01 <93655463+pranavpande01@users.noreply.github.com>
2026-01-01 23:05:36 +0000

    second_commit

--- agents/agent_a.py
+++ agents/agent_a.py
@@ -2,16 +2,19 @@ import yaml
 from agno.agent import Agent
 from agno.db.sqlite import SqliteDb
 from agno.models.google import Gemini
+import os
+from dotenv import load_dotenv
 
+load_dotenv("/workspaces/insults/.env")
 db = SqliteDb(db_file="memories/insults.db")
 
-with open("config.yaml","r") as f:
+with open("/workspaces/insults/agents/config.yaml","r") as f:
     CONFIG = yaml.safe_load(f)
 
 agent = Agent(
     name="Agent A",
     role="The Insulter",
-    model=Gemini(id="gemini-2.5-flash-lite",temperature=0.91),
+    model=Gemini(id="gemini-2.5-flash",temperature=0.91,api_key=os.getenv("GOOGLE_API_KEY")),
     db=db,
     enable_user_memories=True,
     instructions=CONFIG['instructions_for_a'],
